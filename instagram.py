a1aa999fb5827991565632150649002858099293
Julian Böhm <julian.boehm@gmx.de>
2025-02-26 23:17:13 +0100

    add missing changes

--- instagram.py
+++ instagram.py
@@ -107,7 +107,9 @@ def instagram_post_for_status(status: str) -> bool:
     INSTAGRAM_STORY_CANCELED = os.getenv("INSTAGRAM_STORY_CANCELED") == "true"
     INSTAGRAM_STORY_ENGLISH = os.getenv("INSTAGRAM_STORY_ENGLISH") == "true"
     INSTAGRAM_STORY_GERMAN = os.getenv("INSTAGRAM_STORY_GERMAN") == "true"
+    INSTAGRAM_STORY_CHAT = os.getenv("INSTAGRAM_STORY_CHAT") == "true"
     if status == "canceled" and INSTAGRAM_STORY_CANCELED: return True
     elif status == "english" and INSTAGRAM_STORY_ENGLISH: return True
     elif status == "german" and INSTAGRAM_STORY_GERMAN: return True
+    elif status == "chat" and INSTAGRAM_STORY_CHAT: return True
     return False
